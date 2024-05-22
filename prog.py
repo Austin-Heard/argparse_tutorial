dac1bf9272a604f36249a6ad54b9df1bd61765ae
Austin Heard <amheard0311@outlook.com>
2024-05-22 15:47:07 -0600

    Completes the tutorial w/ additional notes for review

--- prog.py
+++ prog.py
@@ -1,5 +1,22 @@
 import argparse
 
+parser = argparse.ArgumentParser(description="calculate X to the power of Y")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-v", "--verbose", action="store_true")
+group.add_argument("-q", "--quiet", action="store_true")
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+args = parser.parse_args()
+answer = args.x**args.y
+
+if args.quiet:
+    print(answer)
+elif args.verbose:
+    print(f"{args.x} to the power {args.y} equals {answer}")
+else:
+    print(f"{args.x}^{args.y} == {answer}")
+
+"""
 #Example 1
 def concepts():
     parser = argparse.ArgumentParser()
@@ -60,6 +77,7 @@ def getting_more_advanced():
     if args.verbosity >= 1:
         print(f"{args.x}^{args.y} == ", end="")
     print(answer)
+"""
 
 '''
 Example 7
@@ -88,8 +106,91 @@ Namespace(args=[], n=['1', '2', '3'])
 parser.parse_args(['-n', '1', '--', '2', '3'])
 Namespace(args=['2', '3'], n=['1'])
 '''
-'''
+
+"""
 Example 8
         Conflicting options
+    So far, we have been working with two methods of an argparse.ArgumentParser instance. 
+    Let’s introduce a third one, add_mutually_exclusive_group(). It allows for us to specify options that conflict with each other. 
+    Let’s also change the rest of the program so that the new functionality makes more sense: 
+    we’ll introduce the --quiet option, which will be the opposite of the --verbose one:
 
-'''
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-v", "--verbose", action="store_true")
+group.add_argument("-q", "--quiet", action="store_true")
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+args = parser.parse_args()
+answer = args.x**args.y
+
+if args.quiet:
+    print(answer)
+elif args.verbose:
+    print(f"{args.x} to the power {args.y} equals {answer}")
+else:
+    print(f"{args.x}^{args.y} == {answer}")
+
+    That should be easy to follow. I’ve added that last output so you can see the sort of flexibility you get, i.e. 
+    mixing long form options with short form ones.
+    Before we conclude, you probably want to tell your users the main purpose of your program, 
+    just in case they don’t know:
+
+parser = argparse.ArgumentParser(description="calculate X to the power of Y")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-v", "--verbose", action="store_true")
+group.add_argument("-q", "--quiet", action="store_true")
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+args = parser.parse_args()
+answer = args.x**args.y
+
+if args.quiet:
+    print(answer)
+elif args.verbose:
+    print(f"{args.x} to the power {args.y} equals {answer}")
+else:
+    print(f"{args.x}^{args.y} == {answer}")
+
+    Note that slight difference in the usage text. 
+    Note the [-v | -q], which tells us that we can either use -v or -q, 
+    but not both at the same time:
+"""
+
+"""
+Example 9
+                How to translate the argparse output
+    The output of the argparse module such as its help text and error messages are all made translatable using the get text module. 
+    This allows applications to easily localize messages produced by argparse. See also Internationalizing your programs and modules.
+    For instance, in this argparse output:
+
+python prog.py --help
+usage: prog.py [-h] [-v | -q] x y
+
+calculate X to the power of Y
+
+positional arguments:
+  x              the base
+  y              the exponent
+
+options:
+  -h, --help     show this help message and exit
+  -v, --verbose
+  -q, --quiet
+
+    The strings usage:, positional arguments:, options: and show this help message and exit are all translatable.
+    In order to translate these strings, they must first be extracted into a .po file. For example, using Babel, run this command:
+
+pybabel extract -o messages.po /usr/lib/python3.12/argparse.py
+
+    This command will extract all translatable strings from the argparse module and output them into a file named messages.po. 
+    This command assumes that your Python installation is in /usr/lib.
+    You can find out the location of the argparse module on your system using this script:
+
+import argparse
+print(argparse.__file__)
+
+    Once the messages in the .po file are translated and the translations are installed using gettext, 
+    argparse will be able to display the translated messages.
+    To translate your own strings in the argparse output, use gettext.
+"""
